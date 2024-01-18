e2ae338f24e88d30d067038bd60f5e14c05ddb29
andres <andres@ANDRESDF3F>
2024-01-18 11:34:23 -0500

    Add project files.

---
+++ console-calculator-2.cpp
@@ -0,0 +1,22 @@
+// console-calculator-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Calculator {
+    public:
+        std::string welcomeMessage = "Welcome to the calculator";
+
+        void printTheWelcomeMessage() const {
+            std::cout << welcomeMessage << std::endl;
+        };
+
+};
+
+int main()
+{
+
+    while (1) {
+
+    }
+}
