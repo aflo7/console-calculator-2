06fc0cc01b9313006e7dd431e2a6188662750bc1
aflo7 <andresflo883@gmail.com>
2024-01-18 12:59:19 -0500

    completed the calculator, added functions prettyPrint and calculate

--- console-calculator-2.cpp
+++ console-calculator-2.cpp
@@ -10,20 +10,46 @@ class Calculator {
             std::cout << welcomeMessage << std::endl;
         };
 
+        double calculate(double firstDigit, char operation, double secondDigit) {
+            switch (operation)
+            {
+
+            case '+':
+                return firstDigit + secondDigit;
+                break;
+            case '-':
+                return firstDigit - secondDigit;
+                break;
+            case '/':
+                return firstDigit / secondDigit;
+                break;
+            case '*':
+                return firstDigit * secondDigit;
+                break;
+            default:
+                break;
+            }
+        }
+
+        void prettyPrint(double result) {
+            std::cout << "The result is " << result << "." << std::endl;
+        }
+
 };
 
 int main()
 {
     Calculator theCalculator;
     theCalculator.printTheWelcomeMessage();
-    
-    int firstDigit;
+    double firstDigit;
+    double secondDigit;
+    char operation;
     std::cout << "Enter the first digit: ";
     std::cin >> firstDigit;
-
-
-    std::cout << "The digit you entered was " << firstDigit << std::endl;
-
-
-
+    std::cout << "Enter the second digit: ";
+    std::cin >> secondDigit;
+    std::cout << "Enter the operation: ";
+    std::cin >> operation;
+    double result = theCalculator.calculate(firstDigit, operation, secondDigit);
+    theCalculator.prettyPrint(result);
 }
